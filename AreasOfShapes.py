832ae605d19127761bd714a625cd214ac9203cb7
saadNoor-Sabir <saadnoorm.sabir@gmail.com>
2024-10-29 22:52:54 +0500

    Added python code that calculate area of shapes
    This is simple python code that calculate area of shapes such as Triangle, Square, Circle and Rectangle in python using Arithmetic operators with proper comments.

---
+++ AreasOfShapes.py
@@ -0,0 +1,26 @@
+# python program to calculate and print area of different shapes
+
+print("Demonstrating calculation of area of different shapes using python operators")
+
+# calculating area of triangle
+base = 3
+height = 4
+areaOfTriangle = (base*height)/2 # area of triangle = (base*height)/2
+print("The area of Triangle is :", areaOfTriangle)
+
+# calculating area of square
+side = 6
+areaOfSquare = side**2 # area of square = side*side
+print("The area of Square is :", areaOfSquare)
+
+# calculating area of circle
+radius = 4
+pie = 3.14
+areaOfCircle = pie*(radius**2) # area of Circle = pie*(radius*radius)
+print("The area of Circle is :", areaOfCircle)
+
+# calculating area of rectangle
+length = 8
+width = 6
+areaOfRectangle = length*width # area of rectangle = lenght*width
+print("The area of Rectangle is :", areaOfRectangle)
