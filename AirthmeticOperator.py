653551bcafb2d5a862a0ef5b6b52a7db251128ae
saadNoor-Sabir <saadnoorm.sabir@gmail.com>
2024-10-29 22:49:04 +0500

    Added python operators code
    This code use python operators to perform basic arithmetic operators such as addition , subtraction, multiplication and division on three variables.

---
+++ AirthmeticOperator.py
@@ -0,0 +1,23 @@
+# python program to demonstrate basic airthmetic operatoes in python
+
+num1 = 32
+num2 = 13
+num3 = 24
+
+print("Sum, difference, product and average calcualtions")
+
+# to calculate Addition of three variables
+sum = num1+num2+num3
+print("The sum of three variables 32, 13, 24 =", sum)
+
+# to calculate Subtraction of three variables
+diff = (num1-num2)-num3
+print(f"The difference of three variables 32, 13, 24 =", diff)
+
+# to calculate Multiplication of three variables
+product = num1*num2*num3
+print(f"The product of three variables 32, 13, 24 =", product)
+
+# to calculate average of three variables
+avrg = (num1+num2+num3)/3
+print(f"The average of three variables 32, 13, 24 =", avrg)
